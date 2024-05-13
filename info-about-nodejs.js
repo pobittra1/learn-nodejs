4c1873431b6b57d8eb0ac328229cad3e3d1084ff
pobi <pobittrachandraroy2001@gmail.com>
2024-05-13 21:28:59 +0600

    write some info data about node js

---
+++ info-about-nodejs.js
@@ -0,0 +1,37 @@
+//node js module are--------------
+/*
+1. os system module
+2. file system module
+3. path module 
+4. HTTP module
+5. url module 
+6. utilities module
+*/
+
+//*node js depends on v8 engine and libuv.
+
+//common js and esm 
+
+//node js supports now esm module (version>=14).
+
+//difference between common js and esm_____________
+//common js
+/*
+1. we can import using- require
+2. we can export using- export/modules.export
+3. syntax is .js
+*/
+//esm
+/*
+1. we can import using- import
+2. we can export using- export default
+3. syntax is- .mjs
+*/
+
+
+//modular system are----------
+/*
+1. local module (we create).
+2. built in module (come with node js).
+3. third party modules (created by others) 
+*/
