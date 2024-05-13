a3c58ea54bea52e94cddaa3f3babaae4c442e81b
pobi <pobittrachandraroy2001@gmail.com>
2024-05-13 21:47:56 +0600

    explore done about local module file system

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-//
+//import data in common js way with destructuring
+const { num, add } = require("./local-1");
+const { num:numTwo, add:addTwo } = require("./local-2");
+
+console.log(add(10,10));
+console.log(addTwo(30,20));
--- local-2.js
+++ local-2.js
@@ -1,7 +1,7 @@
 //declare func
 const add = (param1, param2) => param1 + param2;
 //declare var with value
-const numOne = 10;
+const numOne = 20;
 
 //export data in common js way
 module.exports = {
